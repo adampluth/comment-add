1ab5f6324964dd808c1099b98b437b1259921764
Adam Pluth <adam.pluth@gmail.com>
2018-10-29 14:11:53 -0600

    finished exercise

--- js/app.js
+++ js/app.js
@@ -1,7 +1,50 @@
 var main = function () {
     "use strict";
 
-    window.alert("Hello World");
+    // $('.comment-input button').click(function (event) {
+    //     var $new_comment;
+    //     if ($('.comment-input input').val() !== '') {
+    //         $new_comment = $('<p>').text($('.comment-input input').val());
+    //         $new_comment.hide();
+    //         $('.comments').append($new_comment);
+    //         $new_comment.fadeIn();
+    //         $('.comment-input input').val('');
+    //     }
+    // });
+
+    // $('.comment-input input').keypress(function (event) {
+    //     var $new_comment;
+
+    //     if (event.keyCode === 13) {
+    //         $new_comment = $('<p>').text($('.comment-input input').val());
+    //         $new_comment.hide();
+    //         $('.comments').append($new_comment);
+    //         $new_comment.fadeIn();
+    //         $('.comment-input input').val('');
+    //     }
+    // })
+
+    var addCommentFromInputBox = function () {
+        var $new_comment;
+
+        if ($('.comment-input input').val() !== '') {
+            $new_comment = $('<p>').text($('.comment-input input').val());
+            $new_comment.hide();
+            $('.comments').append($new_comment);
+            $new_comment.fadeIn();
+            $('.comment-input input').val('');
+        }
+    };
+
+    $('.comment-input button').click(function (event) {
+        addCommentFromInputBox();
+    });
+
+    $('.comment-input input').keypress(function (event) {
+        if (event.keyCode === 13) {
+            addCommentFromInputBox();
+        }
+    });
 };
 
 $(document).ready(main);
